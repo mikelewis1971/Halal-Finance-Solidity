3061b8e2303e2861159ee87ac873ef97c221aa53
mikelewis1971 <mikelewis1971@yahoo.com>
2025-03-27 09:51:26 -0400

    Update Qard_Hasan_Contract.sol

--- Qard_Hasan_Contract.sol
+++ Qard_Hasan_Contract.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract QardHasan {
+    address public lender;
+    address public borrower;
+    uint public loanAmount;
+    uint public repaidAmount;
+    bool public loanIssued;
+
+    constructor(address _borrower, uint _amount) {
+        lender = msg.sender;
+        borrower = _borrower;
+        loanAmount = _amount;
+    }
+
+    function issueLoan() external payable {
+        require(msg.sender == lender, "Only lender can issue");
+        require(!loanIssued, "Loan already issued");
+        require(msg.value == loanAmount, "Incorrect amount");
+        loanIssued = true;
+        payable(borrower).transfer(loanAmount);
+    }
+
+    function repay() external payable {
+        require(msg.sender == borrower, "Only borrower can repay");
+        require(loanIssued, "Loan not issued");
+        require(repaidAmount < loanAmount, "Already repaid");
+
+        repaidAmount += msg.value;
+        payable(lender).transfer(msg.value);
+    }
+
+    function isFullyRepaid() public view returns (bool) {
+        return repaidAmount >= loanAmount;
+    }
+}
 
